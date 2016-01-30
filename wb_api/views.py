391a291630c644c63f03be243db3a62a9f6a7f16
simon582 <simonsxq582@gmail.com>
2016-01-30 18:05:29 +0800

    fix result of GET request

--- wb_api/views.py
+++ wb_api/views.py
@@ -3,6 +3,7 @@
 from django.http import HttpResponse
 import os
 import sys
+import json
 reload(sys)
 sys.setdefaultencoding('utf-8')
 
@@ -16,22 +17,39 @@ def record_id(wb_id, max_wb_page, max_cmt_page):
 
 def handle_request(request):
     try:
-        wb_id = request.GET.get('wbid', 'null')
-        max_cmt_page = request.GET.get('max_cmt_page', '0')
+        res_dict = {}
+        res_dict['result'] = 0
+        
         try:
+            max_cmt_page = request.GET.get('max_cmt_page', '0')
             max_wb_page = int(request.GET.get('max_wb_page', '0'))
+            res_dict['max_wb_page'] = max_wb_page
         except:
-            return HttpResponse('max_wb_page must be integer')
+            res_dict['result'] = 1
+            res_dict['desc'] = 'max_wb_page must be integer'
+        
         try:
             max_cmt_page = int(request.GET.get('max_cmt_page', '0'))
+            res_dict['max_cmt_page'] = max_cmt_page
         except:
-            return HttpResponse('max_cmt_page must be integer')
-        if wb_id != 'null':
+            res_dict['result'] = 2
+            res_dict['desc'] = 'max_cmt_page must be integer'
+        
+        wb_id = request.GET.get('wbid', 'null')
+        if wb_id != 'null' and wb_id.strip() != '':
+            res_dict['wb_id'] = wb_id
             record_id(wb_id, max_wb_page, max_cmt_page)
-            return HttpResponse(wb_id + ' has recorded.')
+            res_dict['desc'] = ''
         else:
-            return HttpResponse('Parameter is illegal, please input: wbid=xxx')
+            res_dict['result'] = 3
+            res_dict['desc'] = 'Parameter is illegal, please input: wbid=xxx'
+
+        return HttpResponse(json.dumps(res_dict, ensure_ascii=False))
     except Exception as e:
         print e
-        return HttpResponse('Occur an unexpected error, please connect adminstrator')
+        res_dict = {}
+        res_dict['result'] = 4
+        res_dict['desc'] = 'Occur an unexpected error, please connect adminstrator'
+        return HttpResponse(json.dumps(res_dict, ensure_ascii=False))
+        
 
